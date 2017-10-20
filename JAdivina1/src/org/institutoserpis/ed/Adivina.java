e171dafc8e18d133f19b0311c8c5e26a6d6413e5
adrilc24 <adrilc24@gmail.com>
2017-10-20 09:23:03 +0200

    Añadido contador de Intentos

--- JAdivina1/src/org/institutoserpis/ed/Adivina.java
+++ JAdivina1/src/org/institutoserpis/ed/Adivina.java
@@ -9,22 +9,23 @@ public class Adivina {
 		Scanner tcl = new Scanner(System.in);
 		Random random = new Random();
 		int numeroAleatorio = random.nextInt(1000)+1;
-		int contador = 0;
+		int contadorIntentos = 1;
 		String numero;
 		System.out.println("Introduzca el número del 1 al 1000 ");
 		numero=tcl.nextLine();
 		int numeroUsuario = Integer.parseInt(numero);
 		while (numeroUsuario!=numeroAleatorio) {
 			if (numeroAleatorio < numeroUsuario) 
-				System.out.println("El número aleatorio es menor que el introducido, pruebe de nuevo: ");
+				System.out.printf("El número aleatorio es menor que el introducido, pruebe de nuevo [intento %s]: ",contadorIntentos);
 			else
-					System.out.println("El número aleatorio es mayor que el introducido, pruebe de nuevo:  ");
-			contador++;
+					System.out.printf("El número aleatorio es mayor que el introducido, pruebe de nuevo [intento %s]:  ",contadorIntentos);
+			contadorIntentos++;
 			numero = tcl.nextLine();
 			numeroUsuario = Integer.parseInt(numero);
 		}
+		System.out.println("");
 		System.out.println("El número "+numeroAleatorio+" es correcto");
-		System.out.println("Usted ha necesitado "+contador+" intentos");
+		System.out.println("Usted ha necesitado "+contadorIntentos+" intentos");
 		
 	}
 
