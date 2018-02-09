ae2d084e6e71569786cb1dd4cbe737f824fb78aa
adrilc24 <adrilc24@gmail.com>
2018-02-09 09:23:01 +0100

    My first App

---
+++ CVector/CVector/Vector.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CVector
+{
+	[TestFixture ()]
+	public class Vector
+	{
+		/// <summary>
+		/// Sort the specified v.
+		/// </summary>
+		/// <param name="v">V.</param>
+		public static void Sort(int[] v) {
+			//Array.Sort<int> (v); Solucion simplificada
+		}
+
+		/// <summary>
+		/// Indexs the minimum.
+		/// </summary>
+		/// <returns>The minimum.</returns>
+		/// <param name="v">V.</param>
+		/// <param name="initialIndex">Initial index.</param>
+		public static int IndexMin(int[] v, int initialIndex){
+			return -1;
+		}
+
+		/// <summary>
+		/// Swap the specified v, index and otherIndex.
+		/// </summary>
+		/// <param name="v">V.</param>
+		/// <param name="index">Index.</param>
+		/// <param name="otherIndex">Other index.</param>
+		public static void Swap(int[] v,int index, int otherIndex){
+		}
+			
+	}
+}
+
---
+++ CVector/CVector/VectorTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CVector
+{
+	[TestFixture ()]
+	public class VectorTest
+	{
+		[Test ()]
+		public void Sort ()
+		{
+			int[] v = new {12,9,15,7,13};
+			Vector.Sort(v);
+			CollectionAssert.AreEqual(new int[]{7,9,12,13,15}, v);
+			v = new int[] {7,9,12,13,15};
+			Vector.Sort(v);
+			CollectionAssert.AreEqual(new int[] {7,9,12,13,15}, v);
+
+		}
+		[Test]
+		public void IndexMin(){
+			int[] v = { 12, 9, 15, 7, 13 };
+			int indexMin = Vector.IndexMin (v, 4);
+			Assert.AreEqual (4, indexMin);
+		}
+	}
+}
+
