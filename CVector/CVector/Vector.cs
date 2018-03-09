5641a23d96aadc78a5b9bdc0bd20365656a8e1a2
adrilc24 <adrilc24@gmail.com>
2018-03-09 09:45:20 +0100

    Añadido CVector con Sort

--- CVector/CVector/Vector.cs
+++ CVector/CVector/Vector.cs
@@ -6,11 +6,30 @@ namespace CVector
 	[TestFixture ()]
 	public class Vector
 	{
+//		public static void Sort(int[] v){
+			int n = v.Length;
+			for (int positionIndex = 0; positionIndex < n - 1; positionIndex++) {
+				int indexMin = positionIndex;
+				for (int index = IndexMin + 1; index < n; index++)
+					if (v [index] < v [IndexMin])
+						indexMin = index;
+				int aux = v [positionIndex];
+				v [positionIndex] = v [IndexMin];
+				v [IndexMin] = aux;
+			}
+//		int n = v.Length;
+//		for (int positionIndex = 0; positionIndex < n -1; positionIndex++ ){
+//			int indexMin = indexMin(v, positionIndex);
+//			Swap(v, positionIndex, indexMin);
+//		
+//		}
+		}
+
 		/// <summary>
 		/// Sort the specified v.
 		/// </summary>
 		/// <param name="v">V.</param>
-		public static void Sort(int[] v) {
+		///public static void Sort(int[] v) {
 			//Array.Sort<int> (v); Solucion simplificada
 		}
 
@@ -21,7 +40,12 @@ namespace CVector
 		/// <param name="v">V.</param>
 		/// <param name="initialIndex">Initial index.</param>
 		public static int IndexMin(int[] v, int initialIndex){
-			return -1;
+			if (initialIndex >= v.Length)
+				throw new IndexOutOfRangeException ();
+			for (int index = initialIndex + 1; index < v.Length; index++)
+				if (v[index] < v[IndexMin])
+					IndexMin = index;
+			return IndexMin;
 		}
 
 		/// <summary>
@@ -31,6 +55,9 @@ namespace CVector
 		/// <param name="index">Index.</param>
 		/// <param name="otherIndex">Other index.</param>
 		public static void Swap(int[] v,int index, int otherIndex){
+			int aux = v [index];
+			v [index] = v [otherIndex];
+			v [otherIndex] = aux;
 		}
 			
 	}
--- CVector/CVector/VectorTest.cs
+++ CVector/CVector/VectorTest.cs
@@ -9,7 +9,7 @@ namespace CVector
 		[Test ()]
 		public void Sort ()
 		{
-			int[] v = new {12,9,15,7,13};
+			int[] v = {12,9,15,7,13};
 			Vector.Sort(v);
 			CollectionAssert.AreEqual(new int[]{7,9,12,13,15}, v);
 			v = new int[] {7,9,12,13,15};
@@ -18,10 +18,52 @@ namespace CVector
 
 		}
 		[Test]
+		public void SortEmpty(){
+			int[] v = { };
+			Vector.Sort (v);
+			CollectionAssert.AreEqual (new int[]{ }, v);
+		}
+
+		[Test]
 		public void IndexMin(){
-			int[] v = { 12, 9, 15, 7, 13 };
-			int indexMin = Vector.IndexMin (v, 4);
-			Assert.AreEqual (4, indexMin);
+			int[] v = { 12, 7, 15, 9, 13 };
+			Assert.AreEqual (4, Vector.IndexMin (v, 4));
+			Assert.AreEqual (3, Vector.IndexMin (v, 2));
+			Assert.AreEqual (1, Vector.IndexMin (v, 0));
+
+			Vector.IndexMin (v, -1);
+
+		}
+		[Test]
+		[ExpectedException(typeof(System.IndexOutOfRangeException))]
+		public void IndexMinBadInitialIndexLeft(){
+			int[] v = { 12, 7, 15, 9, 13 };
+			Assert.AreEqual (0, Vector.IndexMin (v, -1));
+		}
+		[Test]
+		[ExpectedException(typeof(System.IndexOutOfRangeException))]
+		public void IndexMinBadInitialIndexRight(){
+			int[] v = { 12, 7, 15, 9, 13 };
+			Assert.AreEqual (0, Vector.IndexMin (v, 5));
+		}
+
+		[Test]
+		[ExpectedException(typeof(IndexOutOfRangeException))]
+		public void IndexMinEmpty(){
+			int[] v = { };
+			Vector.IndexMin (v, 0);
+		}
+
+		[Test]
+		public void Swap(){
+			int[] v = { 12, 7, 15, 9, 13 };
+			Vector.Swap (v, 0, 1);
+			CollectionAssert.AreEqual (new int[]{ 7, 12, 15, 9, 13 }, v);
+			v = new int[] { 12, 7, 15, 9, 13 };
+			Vector.Swap (v, 0, 4);
+			CollectionAssert.AreEqual (new int[]{ 13, 7, 15, 9, 12 }, v);
+			Vector.Swap (v, 2, 2);
+			CollectionAssert.AreEqual (new int[]{ 12, 7, 15, 9, 13 }, v);
 		}
 	}
 }
